0a2d2ff2b5f0cbd9776350a6d6989d1d5946d3ca
lasbleic <lasbleis.corentin@gmail.com>
2024-03-17 00:46:26 +0100

    Move body's CSS to PageLayout.module.css

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,11 @@
 import * as React from "react";
 import { PageLayout } from "./shared/Component/PageLayout/PageLayout";
 import { WelcomePage } from "./WelcomePage/WelcomePage";
-import styles from "./App.module.css";
 
 export const App: React.FC = () => {
     return (
-        <div className={styles.container}>
-            <PageLayout>
-                <WelcomePage />
-            </PageLayout>
-        </div>
+        <PageLayout>
+            <WelcomePage />
+        </PageLayout>
     );
 };
