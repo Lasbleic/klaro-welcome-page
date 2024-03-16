47b522f9988c79059bf359e6e2f335b2d0235a57
lasbleic <lasbleis.corentin@gmail.com>
2024-03-17 00:17:45 +0100

    Add Styles to the Header

--- src/App/Header/Header.tsx
+++ src/App/Header/Header.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import logo from "./logo.svg";
+import styles from "./Header.module.css";
 
 export const Header: FC = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <img src={logo} alt="Klaro logo" />
         </header>
     );
