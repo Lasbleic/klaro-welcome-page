cf045f2262996bbbaa68d2a1f6e7fb767b32582c
lasbleic <lasbleis.corentin@gmail.com>
2024-03-16 23:48:28 +0100

    use Rubik Font with css modules to align

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Header } from "./Header/Header";
 import { WelcomePage } from "./Page/WelcomePage";
+import styles from "./App.module.css";
 
 export const App: React.FC = () => {
     return (
-        <>
+        <div className={styles.container}>
             <Header />
             <WelcomePage />
-        </>
+        </div>
     );
 };
