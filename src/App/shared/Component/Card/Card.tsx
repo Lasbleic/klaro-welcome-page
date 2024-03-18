16e2f1f0c29f3acae3841ed6cfd532a95519d652
lasbleic <lasbleis.corentin@gmail.com>
2024-03-18 12:10:26 +0100

    First styling on Cards

--- src/App/shared/Component/Card/Card.tsx
+++ src/App/shared/Component/Card/Card.tsx
@@ -1,16 +1,48 @@
 import { FC } from "react";
+import styles from "./Card.module.css";
 
 interface CardProps {
     title: string;
     illustrationSrc: string;
     buttonLabel: string;
+    type: "primary" | "secondary";
 }
 
 export const Card: FC<CardProps> = (props) => {
+    const { type, ...other } = props;
+
+    const className = `${styles.baseCard} ${type === "primary" && styles.primaryCard} ${
+        type === "secondary" && styles.secondaryCard
+    }`;
+
+    return (
+        <article className={className}>
+            {type === "primary" && <PrimaryCardLayout {...other} />}
+            {type === "secondary" && <SecondaryCardLayout {...other} />}
+        </article>
+    );
+};
+
+const PrimaryCardLayout: FC<Omit<CardProps, "type">> = (props) => {
     const { title, illustrationSrc, buttonLabel } = props;
+
     return (
         <>
-            <h4>{title}</h4>
+            <div>
+                <h4>{title}</h4>
+                <button>{buttonLabel}</button>
+            </div>
+            <img src={illustrationSrc} alt="Card description" />
+        </>
+    );
+};
+
+const SecondaryCardLayout: FC<Omit<CardProps, "type">> = (props) => {
+    const { title, illustrationSrc, buttonLabel } = props;
+
+    return (
+        <>
+            <h5>{title}</h5>
             <img src={illustrationSrc} alt="Card description" />
             <button>{buttonLabel}</button>
         </>
