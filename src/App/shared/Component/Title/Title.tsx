c515a9065fbf8c064f9a9b55a22069c7b3f57dfc
lasbleic <lasbleis.corentin@gmail.com>
2024-03-19 20:11:34 +0100

    Add className to Title to dynamically modify it

--- src/App/shared/Component/Title/Title.tsx
+++ src/App/shared/Component/Title/Title.tsx
@@ -3,17 +3,18 @@ import styles from "./Title.module.css";
 
 interface TitleProps {
     type: "h1" | "h4" | "h5";
+    className?: string;
 }
 
 export const Title: FC<PropsWithChildren<TitleProps>> = (props) => {
-    const { type, children } = props;
+    const { type, className, children } = props;
 
     switch (type) {
         case "h1":
-            return <h1 className={styles.h1}>{children}</h1>;
+            return <h1 className={`${styles.h1} ${className}`}>{children}</h1>;
         case "h4":
-            return <h4 className={styles.h4}>{children}</h4>;
+            return <h4 className={`${styles.h4} ${className}`}>{children}</h4>;
         case "h5":
-            return <h5 className={styles.h5}>{children}</h5>;
+            return <h5 className={`${styles.h5} ${className}`}>{children}</h5>;
     }
 };
